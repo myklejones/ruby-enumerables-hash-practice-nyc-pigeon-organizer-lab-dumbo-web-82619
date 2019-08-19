deaa989be9c2fd22e683b0297082f519860105a5
Mykle jones <mykle.j91@gmail.com>
2019-08-19 01:18:02 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash={}
+  data.each do |key,value|
+    value.each do |new_val,name|
+      name.each do |name|
+      if !hash[name]
+        hash[name]={}
+      end
+      if !hash[name][key]
+        hash[name][key]=[]
+      end
+      hash[name][key]<< new_val.to_s 
+    end
+  end
+end
+hash        
 end
